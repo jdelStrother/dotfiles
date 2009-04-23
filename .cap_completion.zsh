661697de3e397606eb6a7c11d733330c70d8a9f1
Jonathan del Strother <jon.delStrother@bestbefore.tv>
2009-04-23 16:48:40 +0100

    Fixing capistrano completion

--- .cap_completion.zsh
+++ .cap_completion.zsh
@@ -14,7 +14,7 @@ _cap_does_task_list_need_generating () {
 _cap () {
   if [ -f config/deploy.rb ]; then
     if _cap_does_task_list_need_generating; then
-      cap -Tv | cut -d " " -f 2 | sed -e '/^ *$/D' -e '1,2D' >! .cap_tasks
+      cap -vT | cut -d " " -f 2 | sed -e '/^ *$/D' -e '1,2D' >! .cap_tasks
     fi
     compadd `cat .cap_tasks`
   fi
