b57150e67d91a1b51df572a4b2314b1ada4f1ada
Jonathan del Strother <jdelStrother@gmail.com>
2019-05-21 12:47:24 +0100

    Default templates for jsx & ruby

---
+++ emacs/.emacs.d/private/templates/00:.*\.jsx
@@ -0,0 +1,13 @@
+// @flow
+import * as React from "react";
+
+type Props = {};
+type State = {};
+
+class ${1:`(string-inflection-pascal-case-function (file-name-base (buffer-file-name)))`} extends React.Component<Props, State> {
+  render() {
+    return <div>hello world</div>;
+  }
+}
+
+export default $1;
---
+++ emacs/.emacs.d/private/templates/01:.*_spec\.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+#require "rails_helper"
+
+RSpec.describe Foo do
+end
