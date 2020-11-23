628d4f25fc884154ef115a201ccacec8e9156ee2
Jonathan del Strother <jdelStrother@gmail.com>
2020-11-23 10:12:28 +0000

    Use checked-out version of nixpkgs for awscli fix

--- nix/.config/profile.nix
+++ nix/.config/profile.nix
@@ -1,15 +1,17 @@
-# Update with:
+# Updating channel:
+# sudo -H nix-channel --list
+# sudo -H nix-channel --update
+#
+# Update user packages with:
 # nix-env -irf .config/profile.nix
 #
-with import <nixpkgs> { }; {
+with import <nixpkgs> { };
+# https://nixos.wiki/wiki/User:Raboof#using_a_fork_of_a_packaged_project
+let nix-vendor = import(/Users/jon/Developer/vendor/nixpkgs) {
+  config.allowUnfree = true;
+};
+in {
   inherit awslogs coreutils emacsMacport fish git nixfmt nodejs-14_x parallel ruby_2_7 go;
 
-  awscli2 = pkgs.awscli2.overrideAttrs(oldAttrs: rec {
-    postBuild = ''
-      PYTHONPATH=${oldAttrs.src}:$PYTHONPATH
-      ${oldAttrs.src}/scripts/gen-ac-index --index-location $out/${python3.sitePackages}/awscli/data/ac.index
-    '';
-  });
-
-
+  awscli2 = nix-vendor.awscli2;
 }
