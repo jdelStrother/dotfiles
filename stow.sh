758e1454ad8f9eaee6beb6900110a4cb1108acae
Jonathan del Strother <jdelStrother@gmail.com>
2020-12-30 15:19:56 +0000

    nix tips

--- stow.sh
+++ stow.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
-set -x
 if [ "$#" -eq 0 ]; then
-  args="docker emacs fish git tmux tools vim"
+  args="docker emacs fish git nix tmux tools vim"
 else
   args="$@"
 fi
 
-stow -t ~ -v --ignore='(.elc|.DS_Store)' $args
+function is_bin_in_path {
+  builtin type -P "$1" &> /dev/null
+}
+
+if ! is_bin_in_path stow ; then
+  echo "install stow first. Or run:"
+  echo 'nix-shell -p stow --run "./stow.sh ..."'
+  exit 1
+else
+  set -x
+  stow -t ~ -v --ignore='(.elc|.DS_Store)' $args
+fi
