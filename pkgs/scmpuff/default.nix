b8384efdf3867dcf75f0869d69cd581bfd8fec07
Jonathan del Strother <me@delstrother.com>
2024-06-25 10:34:01 +0100

    Replace deprecated vendorSha256

--- pkgs/scmpuff/default.nix
+++ pkgs/scmpuff/default.nix
@@ -4,12 +4,12 @@ pkgs.buildGoModule {
   pname = "scmpuff";
   version = "0.5.0";
 
-  vendorSha256 = "sha256-7WHVSEz3y1nxWfbxkzkfHhINLC8+snmWknHyUUpNy7c=";
+  vendorHash = "sha256-7WHVSEz3y1nxWfbxkzkfHhINLC8+snmWknHyUUpNy7c=";
   src = pkgs.fetchFromGitHub {
     owner = "mroth";
     repo = "scmpuff";
     rev = "8a7a6514486e64c4a94473da91f783b57e6d775d";
-    hash="sha256-+L0W+M8sZdUSCWj9Ftft1gkRRfWMHdxon2xNnotx8Xs";
+    hash = "sha256-+L0W+M8sZdUSCWj9Ftft1gkRRfWMHdxon2xNnotx8Xs";
   };
   checkPhase = "";
 }
