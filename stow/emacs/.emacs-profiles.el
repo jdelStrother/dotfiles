b69ba6f694d1d135c5c095f4260319353630f798
Jonathan del Strother <me@delstrother.com>
2026-08-22 14:40:22 +0100

    Remove some unused junk

--- stow/emacs/.emacs-profiles.el
+++
@@ -1,3 +0,0 @@
-(("default" . ((user-emacs-directory . "~/emacs/doom-emacs")))
- ("spacemacs" . ((user-emacs-directory . "~/emacs/spacemacs")))
- ("doom-emacs" . ((user-emacs-directory . "~/emacs/doom-emacs"))))
